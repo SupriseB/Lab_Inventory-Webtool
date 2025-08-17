a8fa106b6966bcdf70b9b60725757931559afb76
SupriseB <suprisebaloyi17@gmail.com>
2025-08-17 12:29:22 +0200

    Commiting with initial set up and project configuration and user authentication

---
+++ lab_inventory/inventory/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    quantity = models.PositiveIntegerField(default=0)
+    purchase_date = models.DateField()
+    location = models.CharField(max_length=200)
+    reorder_level = models.PositiveIntegerField(default=5)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.quantity})"
